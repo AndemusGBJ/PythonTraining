93349ea73bc7f9b67336b6db82188ddfcb012637
AndemusGBJ <andemus017@outlook.fr>
2023-03-12 12:17:32 +0300

    FTP autmating assignment done

--- Assignments/Assignment_week_3/Automating File Transfer/Automating File Transfer.py
+++ Assignments/Assignment_week_3/Automating File Transfer/Automating File Transfer.py
@@ -17,36 +17,83 @@ Here are the steps you can take to automate this process:
     You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
 
 import ftplib
+import os
+import shutil
+import schedule, time
 
 #Define credentials
 
-FTP_HOST = "ftp.dlptest.com"
-FTP_USER = "dlpuser"
-FTP_PASS = "rNrKYTX9g7z3RgJRmxWuGHbeu"
 
 
-ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS) #Connect to FTP server
-print(ftp.getwelcome()) #Check if connection is successful
+def main():
+    FTP_HOST = "ftp.dlptest.com"
+    FTP_USER = "dlpuser"
+    FTP_PASS = "rNrKYTX9g7z3RgJRmxWuGHbeu"
+    FTP_PORT = 21
+    ftp = ftplib.FTP(FTP_HOST, FTP_USER, FTP_PASS, FTP_PORT) #Connect to FTP server
+    print(ftp.getwelcome()) #Check if connection is successful
 
-ftp.dir()           #Directory listing
-print(ftp.nlst())   #List files in directory
+    ftp.dir()           #Directory listing
+    print(ftp.nlst())   #List files in directory
 
-#I am going to upload files to the FTP server first in a new folder.
+    #I am going to upload files to the FTP server first in a new folder.
 
-#Create a new folder in the directory
+   
 
-ftp.mkd('assignment_week_3')
-ftp.cwd('assignment_week_3') #Change directory to the new folder
-print(ftp.pwd())
+    #This will help to upload 4 files to the FTP server if needed. Theses have been created in advance
+    # for i in range(1,9):
+    #     with open('Automating File Transfer\\files_to_uploaded\\'+'file'+str(i)+'.txt','rb') as f:
+    #         ftp.storbinary('STOR file'+str(i)+'.txt', f)
 
-#Let's upload 4 files to the FTP server. Theses have been created in advance
-for i in range(1,5):
-    with open('file'+str(i)+'.txt','rb') as f:
-        ftp.storbinary('STOR file'+str(i)+'.txt', f)
+    # print(ftp.nlst())
 
-print(ftp.nlst())
+    #Now let's find the directory where the files will be stored
 
+    directoryName = "FTP_files" 
+    if not os.path.exists(directoryName):
+        os.mkdir(directoryName)
+        print("Directory " , directoryName ,  " Created ")
 
+    #Now let's download the files from the FTP server to the created directory
+
+    for file in ftp.nlst():
+        try: 
+            with open(os.path.join(directoryName, file), 'wb') as f:
+                ftp.retrbinary('RETR ' + file, f.write)
+                print("File " , file ,  " downloaded successfully")
+        except:
+            with open("logFile.txt", "a") as log:
+                log.write("File " + file + " did not be downloaded\n")
+            print("File " , file ,  " did not be downloaded")  
+
+    
+    #Now let's move the files from the local directory to the internal network
+    currentDir = os.getcwd()
+    sourceDir = "FTP_files"
+    sourceDir = os.path.join(currentDir, sourceDir)
+
+    destinationDir = ("Internal Network")
+    if not os.path.exists(destinationDir):
+        os.mkdir(destinationDir)
+        print("Directory " , destinationDir ,  " Created ")
+
+    for file in os.listdir(sourceDir):
+        try:
+            shutil.move(os.path.join(sourceDir, file), destinationDir)
+            print("File " , file ,  " moved successfully")
+        except:
+            with open("logFile.txt", "a") as log:
+                log.write("File " + file + " did not be moved\n")
+            print("File " , file ,  " did not be moved")
+    
+    
+
+# Schedule the script to run daily at 14:30
+schedule.every().day.at("14:30").do(main)
+# For keeping the script running while waiting for scheduled tasks
+while True:
+    schedule.run_pending()
+    time.sleep(1)
 
 
 
